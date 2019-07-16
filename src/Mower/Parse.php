fbd9718554ce580578ec913f598da8f8ceae62a1
Guillaume Migeon <guillaume.migeon@gmail.com>
2019-07-16 12:59:45 +0200

    add log to parse

--- src/Mower/Parse.php
+++ src/Mower/Parse.php
@@ -1,22 +1,39 @@
 <?php 
 // src/Mower/Parse.php
 namespace App\Mower;
+use Monolog\Logger;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Processor\UidProcessor;
+use Monolog\Processor\WebProcessor;
+use Monolog\Processor\MemoryUsageProcessor;
+use Monolog\Processor\ProcessIdProcessor;
+use Monolog\Formatter\LineFormatter;
+
 
 class Parse
 {
     private $file;
     private $content;
+    private $logger;
 
+    public function __construct ()
+    {
+        $this->logger = new Logger('logger');
+    }
     public function setFile($file)
     {
-        echo $file.PHP_EOL;
         if (is_file($file))
         {
             $this->setFile= $file;
+            if ($this->getContent() == "") {
+                $this->setContent(file_get_contents($file));
+            }
         }
-        if ($this->getContent() == "") {
-            $this->setContent(file_get_contents($file));
+        else 
+        {
+            $this->logger->bug("file expected");
         }
+        
     }
     public function setContent($content)
     {
